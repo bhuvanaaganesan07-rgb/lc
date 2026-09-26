fedfae26d1e0132dea32b6af95ccda1e8f352b43
bhuvana <bhuvanaaganesan07@gmail.com>
2026-09-26 13:31:24 +0530

    Create d2besttym.py

---
+++ d2besttym.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        min_price=prices[0]
+        max_profit=0
+        for i in range(len(prices)):
+            current_price=prices[i]
+            if current_price<min_price:
+                min_price=current_price
+            profit=current_price-min_price
+            if profit >max_profit:
+                max_profit=profit
+        return max_profit
+            
